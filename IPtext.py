c45b401bd1d0f8d6fb3bb82de9ee8c8933b21270
Ascotbe <33721903+Ascotbe@users.noreply.github.com>
2019-09-02 18:22:47 +0800

    更新http代理池

--- IPtext.py
+++ IPtext.py
@@ -1,65 +1,40 @@
+# !/usr/bin/env python
+# _*_ coding: utf-8 _*_
 import requests
 import re
+from scrapy.selector import Selector
 
+def crawl_ips():
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/59.0.3071.115 Safari/537.36"}
+    for i in range(1, 100):
+        HttpUrl = 'http://www.xicidaili.com/wt/{0}'.format(i)
+        req = requests.get(url=HttpUrl, headers=headers)
+        selector = Selector(text=req.text)
+        HttpAllTrs = selector.xpath('//*[@id="ip_list"]//tr')
 
-class Proxies:
-    def __init__(self):
-        self.proxy_list = []
-        self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/45.0.2454.101 Safari/537.36",
-            'Accept-Encoding': 'gzip, deflate, sdch',
-        }
+        HttpIpLists = []
+        for tr in HttpAllTrs[1:]:#过滤第一个tr标签里面是其他数据
+            HttpIp = tr.xpath('td[2]/text()').extract()[0]
+            HttpPort = tr.xpath('td[3]/text()').extract()[0]
+            #proxy_type = tr.xpath('td[6]/text()').extract()[0].lower()
+            HttpIpLists.append((HttpIp+':'+HttpPort))#存储到httpIP列表里面
 
-    # 爬取西刺代理的国内高匿代理
-    def get_proxy_nn(self):
-        proxy_list = []
-        res = requests.get("http://www.xicidaili.com/nn", headers=self.headers)
-        ip_list = re.findall('<td>(\d+\.\d+\.\d+\.\d+)</td>', res.text)
-        port_list = re.findall('<td>(\d+)</td>', res.text)
-        for ip, port in zip(ip_list, port_list):
-            proxy_list.append(ip + ":" + port)
-        return proxy_list
-
-    # 验证代理是否能用
-    def verify_proxy(self, proxy_list):
-        for proxy in proxy_list:
+        for ip in HttpIpLists:
+            #print(ip)
             proxies = {
-                "http": proxy
+                "http": ip
             }
             try:
-                if requests.get('http://www.baidu.com', proxies=proxies, timeout=2).status_code == 200:
-                    print('success %s' % proxy)
-                    if proxy not in self.proxy_list:
-                        self.proxy_list.append(proxy)
+
+                if requests.get('https://www.baidu.com/', proxies=proxies, timeout=2).status_code == 200:
+                    print('success %s' % ip)
+                    # if proxy not in proxy_list:#如果代理IP不在列表里面就传到列表里
+                    #     proxy_list.append(proxy)
             except:
-                print('fail %s' % proxy)
-
-    # 保存到proxies.txt里
-    def save_proxy(self):
-        # 验证代理池中的IP是否可用
-        print("开始清洗代理池...")
-        with open("proxies.txt", 'a', encoding="utf-8") as f:
-            txt = f.read()
-        # 判断代理池是否为空
-        if txt != '':
-            self.verify_proxy(txt.strip().split('\n'))
-        else:
-            print("代理池为空！\n")
-        print("开始存入代理池...")
-        # 把可用的代理添加到代理池中
-        with open("proxies.txt", 'a', encoding="utf-8") as f:
-            for proxy in self.proxy_list:
-                f.write(proxy + "\n")
+                pass
 
 
-if __name__ == '__main__':
-    p = Proxies()
-    results = p.get_proxy_nn()
-    print("爬取到的代理数量", len(results))
-    print("开始验证：")
-    p.verify_proxy(results)
-    print("验证完毕：")
-    print("可用代理数量：", len(p.proxy_list))
-    p.save_proxy()
+crawl_ips()
 

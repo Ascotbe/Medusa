cab441a13cbb5ae958e7f4f9edbc03ef050ed613
Ascotbe <869335729@qq.com>
2019-09-29 06:51:25 +0800

    v0.22:recycle:

---
+++ InformationDisclosure/JetBrains.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+import urllib
+import requests
+
+def UrlProcessing(url):
+    if url.startswith("http"):#判断是否有http头，如果没有就在下面加入
+        res = urllib.parse.urlparse(url)
+    else:
+        res = urllib.parse.urlparse('http://%s' % url)
+    return res.scheme, res.hostname, res.port
+
+payload = "/.idea/workspace.xml"
+def medusa(Url,RandomAgent,ProxyIp):
+
+    scheme, url, port = UrlProcessing(Url)
+    if port is None and scheme == 'https':
+        port = 443
+    elif port is None and scheme == 'http':
+        port = 80
+    else:
+        port = port
+    global resp
+    global resp2
+    try:
+        payload_url = scheme+"://"+url+payload
+        headers = {
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept': '*/*',
+            'User-Agent': RandomAgent,
+        }
+        #s = requests.session()
+        if ProxyIp!=None:
+            proxies = {
+                # "http": "http://" + str(ProxyIps) , # 使用代理前面一定要加http://或者https://
+                "http": "http://" + str(ProxyIp)
+            }
+            resp = requests.get(payload_url, headers=headers, proxies=proxies, timeout=5, verify=False)
+        elif ProxyIp==None:
+            resp = requests.get(payload_url,headers=headers, timeout=5, verify=False)
+        con = resp.text
+        code = resp.status_code
+        if code==200 and con.lower().find('<?xml version=')!=-1 and con.lower().find('project version')!=-1:
+            Medusa = "{} 存在JetBrains IDE workspace.xml文件泄露漏洞\r\n漏洞详情:\r\nPayload:{}\r\n".format(url, payload_url)
+            return (Medusa)
+    except Exception as e:
+        pass

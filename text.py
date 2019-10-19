8019ea660cce960024d84981df330ff231ef2ae2
Ascotbe <869335729@qq.com>
2019-10-20 00:07:20 +0800

    v0.30:green_heart:

--- text.py
+++ text.py
@@ -1,5 +1,13 @@
-import requests
-def Medusa():
-    s=requests.session()
-    get=s.get("www.ascotbe.com")
-    get.close()
+import pymysql  #导入 pymysql
+
+db= pymysql.connect(host="localhost",user="root",password="zhouyuchen",db="medusa",port=3306)
+
+# 使用cursor()方法获取操作游标
+cur = db.cursor()
+
+sql_insert ="""insert into vulnerability values(4,'liu','1d','12d34','12234','1234','1234')"""
+
+
+cur.execute(sql_insert)
+#提交
+db.commit()

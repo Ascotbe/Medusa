be934b74d2375bec3d09852c62eb29a93ca6b39c
Ascotbe <869335729@qq.com>
2019-11-25 22:27:53 +0800

    v0.40XL:palm_tree:

--- MedusaWeb/deploy/MedusaWeb.py
+++ MedusaWeb/deploy/MedusaWeb.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from .tasks import *#导入异步处理中声明过的函数
 import threading
-
+import json
 
 def api(request):
     return JsonResponse({"result": 0, "msg": "执行成功"})
@@ -30,8 +30,13 @@ def independent_scan(request):
     independent.delay(independent_url,independent_mod_name)
     return JsonResponse({"result": 0, "msg": "执行成功"})
 
-def result_query(request):
-    user_token=request.POST.get("task")
-    pid=request.POST.get("id")#SQL中唯一的id值
-    info=result.delay(user_token,pid)
-    return JsonResponse(info)#用JS返回值具体的等查询语句改好写
+def result_query(request):#通过ID查询API
+    user_token=request.GET.get("task")
+    pid=request.GET.get("id")#SQL中唯一的id值
+    name=request.GET.get("name")#通过漏洞名字查找
+    user=request.GET.get("user")#通过用户ID查找
+    rank=request.GET.get("rank")#通过危害等级查找
+    if rank!=None:#判断不为空调用相对应的函数
+        print(rank)
+    info=result.delay(user_token,pid).result
+    return HttpResponse(info)#用JS返回值具体的等查询语句改好写

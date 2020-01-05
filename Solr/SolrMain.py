aa459d849f9b41a3a3a0fc03664a47f55daba915
Ascotbe <869335729@qq.com>
2020-01-05 19:31:05 +0800

    v0.47XLL

--- Solr/SolrMain.py
+++ Solr/SolrMain.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # _*_ coding: utf-8 _*_
 from Solr import SolrVelocityTemplateRemoteCodeExecutionVulnerability
+from Solr import SolrVelocityTemplateRemoteCodeExecutionVulnerability2
 import ClassCongregation
 from tqdm import tqdm
 def Main(Url,FileName,Values,ProxyIp):
     WriteFile = ClassCongregation.WriteFile(FileName)  # 声明调用类集合中的WriteFile类,并传入文件名字(这一步是必须的)
     ua=ClassCongregation.UserAgentS(Values)#传入用户输入用户指定的浏览器头
     RandomAgent=ua.UserAgent()#获取生成的头文件
-    Medusa = [SolrVelocityTemplateRemoteCodeExecutionVulnerability.medusa(Url,RandomAgent,ProxyIp)]
+    Medusa = [SolrVelocityTemplateRemoteCodeExecutionVulnerability.medusa(Url,RandomAgent,ProxyIp),SolrVelocityTemplateRemoteCodeExecutionVulnerability2.medusa(Url,RandomAgent,ProxyIp)]
     try:
         for i in tqdm(Medusa, ascii=True, desc="Solr plugin progress"):
             WriteFile.Write(str(i))

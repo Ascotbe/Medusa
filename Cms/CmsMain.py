91dc06d9b93c8e599a557a2725f6b6608f25580f
Ascotbe <869335729@qq.com>
2020-01-10 21:12:38 +0800

    v0.50X

--- Cms/CmsMain.py
+++ Cms/CmsMain.py
@@ -7,6 +7,7 @@ from Cms.OneCaitong import OneCaitong
 from Cms.Pboot import Pboot
 from Cms.FiveClib import FiveClib
 from Cms._74CMS import _74CMS
+from Cms.Phpweb import Phpweb
 def Main(Url,FileName,Values,ProxyIp):
     try:
         SecCms.Main(Url,FileName,Values,ProxyIp)
@@ -30,5 +31,9 @@ def Main(Url,FileName,Values,ProxyIp):
         pass
     try:
         _74CMS.Main(Url,FileName,Values,ProxyIp)
+    except:
+        pass
+    try:
+        Phpweb.Main(Url,FileName,Values,ProxyIp)
     except:
         pass

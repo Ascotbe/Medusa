56840db689b5ef6f0f3bde897f429eb0dd5885b2
Ascotbe <869335729@qq.com>
2020-01-13 22:19:24 +0800

    v0.51X:palm_tree:

--- Citrix/CitrixMain.py
+++ Citrix/CitrixMain.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # _*_ coding: utf-8 _*_
 from Citrix import CitrixGatewayPathTraversalVulnerability
+from Citrix import CitrixRemoteCodeExecutionVulnerability
 import ClassCongregation
 from tqdm import tqdm
 
@@ -9,7 +10,8 @@ def Main(Url,FileName,Values,ProxyIp):
     WriteFile = ClassCongregation.WriteFile(FileName)  # 声明调用类集合中的WriteFile类,并传入文件名字(这一步是必须的)
     ua=ClassCongregation.UserAgentS(Values)#传入用户输入用户指定的浏览器头
     RandomAgent=ua.UserAgent()#获取生成的头文件
-    Medusa = [CitrixGatewayPathTraversalVulnerability.medusa(Url, RandomAgent, ProxyIp), ]
+    Medusa = [CitrixGatewayPathTraversalVulnerability.medusa(Url, RandomAgent, ProxyIp),
+              CitrixRemoteCodeExecutionVulnerability.medusa(Url, RandomAgent, ProxyIp),]
     try:
         for i in tqdm(Medusa, ascii=True, desc="Citrix plugin progress"):
             WriteFile.Write(str(i))

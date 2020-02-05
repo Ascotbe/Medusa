6bd31954fb414d0e2fe72fe2dd344bc6fa9c4384
Ascotbe <869335729@qq.com>
2020-02-05 12:48:43 +0800

    v0.56:palm_tree:

--- Cms/_74CMS/_74CMS.py
+++ Cms/_74CMS/_74CMS.py
@@ -18,11 +18,13 @@ from Cms._74CMS import _74CMSSQLInjectionVulnerabilityExists14
 from Cms._74CMS import _74CMSSQLInjectionVulnerabilityExists15
 
 
-import ClassCongregation
+from ClassCongregation import WriteFile,UserAgentS
 from tqdm import tqdm
+
+
 def Main(Url,FileName,Values,ProxyIp):
-    WriteFile = ClassCongregation.WriteFile(FileName)  # 声明调用类集合中的WriteFile类,并传入文件名字(这一步是必须的)
-    ua=ClassCongregation.UserAgentS(Values)#传入用户输入用户指定的浏览器头
+    WriteFiles = WriteFile(FileName)  # 声明调用类集合中的WriteFile类,并传入文件名字(这一步是必须的)
+    ua=UserAgentS(Values)#传入用户输入用户指定的浏览器头
     RandomAgent=ua.UserAgent()#获取生成的头文件
     Medusa = [_74CMSThereIsReflectiveXSSVulnerability.medusa(Url,RandomAgent,ProxyIp),
               _74CMSSQLInjectionVulnerabilityExists.medusa(Url,RandomAgent,ProxyIp),
@@ -42,6 +44,6 @@ def Main(Url,FileName,Values,ProxyIp):
               _74CMSSQLInjectionVulnerabilityExists15.medusa(Url, RandomAgent, ProxyIp),]
     try:
         for i in tqdm(Medusa, ascii=True, desc="74CMS plugin progress"):
-            WriteFile.Write(str(i))
+            WriteFiles.Write(str(i))
     except:
         pass
--- Rails/RailsMain.py
+++ Rails/RailsMain.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # _*_ coding: utf-8 _*_
-import ClassCongregation
 from Rails import RubyOnRailsArbitraryFileReading
+from ClassCongregation import WriteFile,UserAgentS
 from tqdm import tqdm
 def Main(Url,FileName,Values,ProxyIp):
-    WriteFile = ClassCongregation.WriteFile(FileName)  # 声明调用类集合中的WriteFile类,并传入文件名字(这一步是必须的)
-    ua=ClassCongregation.UserAgentS(Values)#传入用户输入用户指定的浏览器头
+    WriteFiles = WriteFile(FileName)  # 声明调用类集合中的WriteFile类,并传入文件名字(这一步是必须的)
+    ua=UserAgentS(Values)#传入用户输入用户指定的浏览器头
     RandomAgent=ua.UserAgent()#获取生成的头文件
     Medusa = [RubyOnRailsArbitraryFileReading.medusa(Url,RandomAgent,ProxyIp)]
     try:
         for i in tqdm(Medusa, ascii=True, desc="Rails plugin progress"):
-            WriteFile.Write(str(i))
+            WriteFiles.Write(str(i))
     except:
         pass
 

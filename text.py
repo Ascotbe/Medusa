a2481b6d7521fa8b15e8e8bb8394daad540a24e7
Ascotbe <869335729@qq.com>
2020-02-06 16:09:07 +0800

    v0.56XL:palm_tree:

--- text.py
+++
@@ -1,7 +0,0 @@
-from ClassCongregation import UrlProcessing
-import ClassCongregation
-a=UrlProcessing().result("www.baidu.com:8081")
-ClassCongregation.UrlProcessing().result("www.baidu.com:8081")
-scheme, url, port = a
-print(scheme)
-print(url,port)

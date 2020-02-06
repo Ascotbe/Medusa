7c62139134b1e2de5faa8f18864f53d243a72aea
KpLi0rn <563164617@qq.com>
2020-02-06 09:53:15 +0800

    v0.56:palm_tree:

---
+++ Cms/CTSCMS/CTSCMS.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+from Cms.CTSCMS import CTSCMSSQLInjectionVulnerability
+from ClassCongregation import WriteFile,UserAgentS
+from tqdm import tqdm
+def Main(Url,FileName,Values,ProxyIp):
+    WriteFiles = WriteFile(FileName)  # 声明调用类集合中的WriteFile类,并传入文件名字(这一步是必须的)
+    ua=UserAgentS(Values)#传入用户输入用户指定的浏览器头
+    RandomAgent=ua.UserAgent()#获取生成的头文件
+    Medusa = [CTSCMSSQLInjectionVulnerability.medusa(Url,RandomAgent,ProxyIp),
+              ]
+    try:
+        for i in tqdm(Medusa, ascii=True, desc="CuteCMS plugin progress"):
+            WriteFiles.Write(str(i))
+    except:
+        pass

09474077440103171c32de72b44e2012eb554e58
Ascotbe <869335729@qq.com>
2020-02-20 16:36:40 +0800

    v0.71.3:palm_tree:

--- Kibana/KibanaMain.py
+++ Kibana/KibanaMain.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # _*_ coding: utf-8 _*_
 from Kibana import KibanaArbitraryFileReadVulnerability
+from Kibana import KibanaRemoteCommandExecutionVulnerability
 import time
 def Main(ThreadPool,Url,Values,ProxyIp):
     ThreadPool.Append(KibanaArbitraryFileReadVulnerability.medusa,Url,Values,ProxyIp)
+    ThreadPool.Append(KibanaRemoteCommandExecutionVulnerability.medusa, Url, Values, ProxyIp)
     print("\033[1;40;32m[ + ] Kibana component payload successfully loaded\033[0m")
     time.sleep(0.5)

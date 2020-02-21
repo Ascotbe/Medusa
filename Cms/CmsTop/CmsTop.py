69e4e063e9b932e100661d51c4d2fd793243acc6
KpLi0rn <563164617@qq.com>
2020-02-21 21:30:51 +0800

    v0.71.6:palm_tree:

--- Cms/CmsTop/CmsTop.py
+++ Cms/CmsTop/CmsTop.py
@@ -2,9 +2,11 @@
 # _*_ coding: utf-8 _*_
 from Cms.CmsTop import CmsTopRemoteCodeExecution
 from Cms.CmsTop import CmsTopSQLInjectionVulnerability
+from Cms.CmsTop import CmsTopPathDisclosureVulnerability
 import time
 def Main(ThreadPool,Url,Values,ProxyIp):
     ThreadPool.Append(CmsTopRemoteCodeExecution.medusa, Url, Values, ProxyIp)
     ThreadPool.Append(CmsTopSQLInjectionVulnerability.medusa, Url, Values, ProxyIp)
+    ThreadPool.Append(CmsTopPathDisclosureVulnerability.medusa, Url, Values, ProxyIp)
     print("\033[1;40;32m[ + ] CmsTop component payload successfully loaded\033[0m")
     time.sleep(0.5)

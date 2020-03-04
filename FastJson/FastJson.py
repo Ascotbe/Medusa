df842f84dee651d30fdb9b6a9fa32afcb9b14c08
Ascotbe <869335729@qq.com>
2020-03-04 20:06:39 +0800

    v0.73.5:palm_tree:

--- FastJson/FastJson.py
+++ FastJson/FastJson.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # _*_ coding: utf-8 _*_
+#改漏洞无特定的URL位置，可能是不通主键不同的URL
+#里面的URL应该改为不处理，传入什么就打，或者跑全目录打
 from FastJson import FastjsonDeserializationRemoteCodeExecutionVulnerability
 from FastJson import FastjsonDeserializationRemoteCodeExecutionVulnerability2
 from ClassCongregation import Prompt

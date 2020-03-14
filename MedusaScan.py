ce697deec1e42fd15b24b0a33da1e0dcb4b7f33a
Ascotbe <869335729@qq.com>
2020-03-14 12:08:42 +0800

    v0.74.2:palm_tree:

--- MedusaScan.py
+++ MedusaScan.py
@@ -30,7 +30,7 @@ parser = argparse.ArgumentParser()#description="xxxxxx")
 #UrlGroup = parser.add_mutually_exclusive_group()#定义一个互斥参数组
 #UrlGroup .add_argument("-q", "--quiet", action="store_true")#增加到互斥参数组里面去
 parser.add_argument('-u','--url',type=str,help="Target url")
-#parser.add_argument('-p','--Proxy',help="Whether to enable the global proxy function",action="store_true")
+parser.add_argument('-m','--Module',type=str,help="Scan an application individually")
 parser.add_argument('-a','--agent',type=str,help="Specify a header file or use a random header")
 parser.add_argument('-t','--ThreadNumber',type=int,help="Set the number of threads, the default number of threads 15.")
 parser.add_argument('-f','--InputFileName',type=str,help="Specify bulk scan file batch scan")
@@ -61,12 +61,12 @@ def BoomDB(Url,SqlUser,SqlPasswrod,InputFileName):
 def NmapScan(url):#Nmap扫描这样就可以开多线程了
     ClassCongregation.NmapScan(url).ScanPort()#调用Nmap扫描类
 
-def InitialScan(ThreadPool,InputFileName,Url,ProxyIp):
+def InitialScan(ThreadPool,InputFileName,Url,ProxyIp,Module):
     try:
         if InputFileName==None:
             try:
                 print("\033[1;40;32m[ + ] Scanning target domain:\033[0m" + "\033[1;40;33m {}\033[0m".format(Url))
-                San(ThreadPool,Url,Values,ProxyIp)
+                San(ThreadPool,Url,Values,ProxyIp,Module)
                 #ThreadPool.NmapAppend(NmapScan,Urls)#把Nmap放到多线程中
                 #print("\033[1;40;32m[ + ] NmapScan component payload successfully loaded\033[0m")
 
@@ -78,7 +78,7 @@ def InitialScan(ThreadPool,InputFileName,Url,ProxyIp):
                     for UrlLine in f:#设置头文件使用的字符类型和开头的名字
                         try:
                             print("\033[1;40;32m[ + ] In batch scan, the current target is:\033[0m"+"\033[1;40;33m {}\033[0m".format(UrlLine.replace('\n', '')))
-                            San(ThreadPool,UrlLine,Values,ProxyIp)
+                            San(ThreadPool,UrlLine,Values,ProxyIp,Module)
                             #ThreadPool.NmapAppend(NmapScan,Urls)#把Nmap放到多线程中
                             #print("\033[1;40;32m[ + ] NmapScan component payload successfully loaded\033[0m")
                         except KeyboardInterrupt as e:
@@ -88,23 +88,58 @@ def InitialScan(ThreadPool,InputFileName,Url,ProxyIp):
     except:
         print("\033[1;40;31m[ ! ] Please enter the correct file path!\033[0m")
 
-def San(ThreadPool,Url,Values,ProxyIp):
+def San(ThreadPool,Url,Values,ProxyIp,Module):
     #POC模块存进多线程池，这样如果批量扫描会变快很多
-    Struts2Main.Main(ThreadPool,Url,Values,ProxyIp)# 调用Struts2主函数
-    ConfluenceMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用 Confluence主函数
-    NginxMain.Main(ThreadPool,Url,Values,ProxyIp)#调用Nginx主函数
-    ApacheMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用Apache主函数
-    PHPStudy.Main(ThreadPool,Url,Values,ProxyIp)# 调用Php主函数
-    CmsMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用Cms主函数
-    OaMian.Main(ThreadPool,Url,Values,ProxyIp)# 调用OA主函数
-    JenkinsMain.Main(ThreadPool,Url,Values,ProxyIp)  # 调用Jenkins主函数
-    Harbor.Main(ThreadPool, Url, Values, ProxyIp)# 调用Harbor主函数
-    RailsMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用RailsMain主函数
-    KibanaMain.Main(ThreadPool,Url,Values,ProxyIp) # 调用KibanaMain主函数
-    CitrixMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用CitrixMain主函数
-    MongoMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用MongoMain主函数
-    SpringMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用SpringMain主函数
-    FastJson.Main(ThreadPool,Url,Values,ProxyIp)# 调用FastJson主函数
+    if Module==None:
+        Struts2Main.Main(ThreadPool,Url,Values,ProxyIp)# 调用Struts2主函数
+        ConfluenceMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用 Confluence主函数
+        NginxMain.Main(ThreadPool,Url,Values,ProxyIp)#调用Nginx主函数
+        ApacheMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用Apache主函数
+        PHPStudy.Main(ThreadPool,Url,Values,ProxyIp)# 调用Php主函数
+        CmsMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用Cms主函数
+        OaMian.Main(ThreadPool,Url,Values,ProxyIp)# 调用OA主函数
+        JenkinsMain.Main(ThreadPool,Url,Values,ProxyIp)  # 调用Jenkins主函数
+        Harbor.Main(ThreadPool, Url, Values, ProxyIp)# 调用Harbor主函数
+        RailsMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用RailsMain主函数
+        KibanaMain.Main(ThreadPool,Url,Values,ProxyIp) # 调用KibanaMain主函数
+        CitrixMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用CitrixMain主函数
+        MongoMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用MongoMain主函数
+        SpringMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用SpringMain主函数
+        FastJson.Main(ThreadPool,Url,Values,ProxyIp)# 调用FastJson主函数
+    elif Module == "Struts2":
+        Struts2Main.Main(ThreadPool, Url, Values, ProxyIp)  # 调用Struts2主函数
+    elif Module == "Confluence":
+        ConfluenceMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用 Confluence主函数
+    elif Module == "Nginx":
+        NginxMain.Main(ThreadPool,Url,Values,ProxyIp)#调用Nginx主函数
+    elif Module == "Apache":
+        ApacheMain.Main(ThreadPool, Url, Values, ProxyIp)  # 调用Apache主函数
+    elif Module == "PHPStudy":
+        PHPStudy.Main(ThreadPool,Url,Values,ProxyIp)# 调用Php主函数
+    elif Module == "Cms":
+        CmsMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用Cms主函数
+    elif Module=="Oa":
+        OaMian.Main(ThreadPool,Url,Values,ProxyIp)# 调用OA主函数
+    elif Module=="Jenkins":
+        JenkinsMain.Main(ThreadPool,Url,Values,ProxyIp)  # 调用Jenkins主函数
+    elif Module=="Harbor":
+        Harbor.Main(ThreadPool, Url, Values, ProxyIp)# 调用Harbor主函数
+    elif Module=="Rails":
+        RailsMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用RailsMain主函数
+    elif Module=="Kibana":
+        KibanaMain.Main(ThreadPool,Url,Values,ProxyIp) # 调用KibanaMain主函数
+    elif Module=="Citrix":
+        CitrixMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用CitrixMain主函数
+    elif Module == "Mongo":
+        MongoMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用MongoMain主函数
+    elif Module == "Spring":
+        SpringMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用SpringMain主函数
+    elif Module == "FastJson":
+        FastJson.Main(ThreadPool,Url,Values,ProxyIp)# 调用FastJson主函数
+    else:
+        print("\033[1;40;31m[ ! ] Please enter the correct scan module name\033[0m")
+        os._exit(0)  # 直接退出整个函数
+
     ThreadPool.Start(ThreadNumber)#启动多线程
 
 def SubdomainCrawling(Url,SubdomainJudge):#开启子域名函数
@@ -130,7 +165,7 @@ if __name__ == '__main__':
     Values=args.agent#判断是否使用随机头，判断写在Class里面
     SqlPasswrod=args.SqlPasswrod#传入爆破数据库的密码文件
     SqlUser = args.SqlUser#传入爆破数据库的账号文件
-    #Proxy=args.Proxy#不需要传入参数如果开启只需要-p
+    Module=args.Module#单独模块扫描功能
     SubdomainEnumerate=args.SubdomainEnumerate #开启深度子域名枚举，巨TM耗时间
     Subdomain=args.Subdomain#开启子域名枚举
     ThreadNumber=args.ThreadNumber#要使用的线程数默认15
@@ -160,7 +195,7 @@ if __name__ == '__main__':
     elif Subdomain==True:
         SubdomainJudge = "b"
         ThreadPool.SubdomainAppend(SubdomainCrawling, Url,SubdomainJudge)
-    InitialScan(ThreadPool,InputFileName, Url, ProxyIp)#最后启动主扫描函数，这样如果多个IP的话优化速度，里面会做url或者url文件的判断
+    InitialScan(ThreadPool,InputFileName, Url, ProxyIp,Module)#最后启动主扫描函数，这样如果多个IP的话优化速度，里面会做url或者url文件的判断
 
 
 
--- text.py
+++ text.py
@@ -1,3 +1,5 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
 # if __name__ == '__main__':
 #     UrlList=[]
 #     ThredList=[]

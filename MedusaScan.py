81e3b600c62a5c85d36725b3cf7ce56d2961d8e4
Ascotbe <869335729@qq.com>
2020-03-14 12:40:52 +0800

    v0.74.3:palm_tree:

--- MedusaScan.py
+++ MedusaScan.py
@@ -92,6 +92,8 @@ def San(ThreadPool,Url,Values,ProxyIp,Module):
     #POC模块存进多线程池，这样如果批量扫描会变快很多
     ModName=["Struts2","Confluence","Nginx","Apache","PHPStudy","Cms","Oa","Jenkins","Harbor","Rails","Kibana","Citrix","Mongo","Spring","FastJson"]
     if Module==None:
+        print("\033[1;40;32m[ + ] Scanning across modules:\033[0m" + "\033[1;40;35m {}             \033[0m".format(
+            Module))
         Struts2Main.Main(ThreadPool,Url,Values,ProxyIp)# 调用Struts2主函数
         ConfluenceMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用 Confluence主函数
         NginxMain.Main(ThreadPool,Url,Values,ProxyIp)#调用Nginx主函数
@@ -108,7 +110,7 @@ def San(ThreadPool,Url,Values,ProxyIp,Module):
         SpringMain.Main(ThreadPool,Url,Values,ProxyIp)# 调用SpringMain主函数
         FastJson.Main(ThreadPool,Url,Values,ProxyIp)# 调用FastJson主函数
     if Module != None and Module in ModName:
-        print("\033[1;40;32m[ + ] Loading attack module:\033[0m"+"\033[1;40;35m {}             \033[0m".format(Module))
+        print("\033[1;40;32m[ + ] The separate scan module is:\033[0m"+"\033[1;40;35m {}             \033[0m".format(Module))
         if Module == "Struts2":
             Struts2Main.Main(ThreadPool, Url, Values, ProxyIp)  # 调用Struts2主函数
         if Module == "Confluence":

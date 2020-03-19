b18ba009ea0f742928f2e484da0041a7a437164b
Asc0t6e <33721903+Ascotbe@users.noreply.github.com>
2020-03-19 10:52:00 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,10 @@ NICKNAME = {'莎酱'}
 HOST = '0.0.0.0'
 PORT = 8080
 WeatherKey=""
-TULING_API_KEY = ''
-MedusaWebUser=""
-MedusaWebpasswd=""
+TULING_API_KEY =""
+medusa_web_user=""
+medusa_web_passwd=""
+#没有的可以去ceye中申请，http://ceye.io/
+#如果没有改为你的Key会导致有些远程命令执行无法检测
+dns_log_url="XXXXX.ceye.io"
+dns_log_key="XXXXXXXXXXXXXXXXXXXXXXXX"

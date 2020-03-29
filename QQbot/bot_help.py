8a993457ba7c66ec70e63f7dcc5cd4123aafb824
Ascotbe <869335729@qq.com>
2020-03-29 20:51:07 +0800

    v0.77:palm_tree:

--- QQbot/bot_help.py
+++ QQbot/bot_help.py
@@ -1,20 +1,26 @@
 from nonebot import on_command, CommandSession,message
 
 help_list = """
+[CQ:emoji,id=127773][CQ:emoji,id=127773][CQ:emoji,id=127773][CQ:emoji,id=127773][CQ:emoji,id=127773][CQ:emoji,id=127773][CQ:emoji,id=127773][CQ:emoji,id=127773][CQ:emoji,id=127773][CQ:emoji,id=127773]
 1.天气查询 城市
-天气 北京
+[CQ:emoji,id=9757]天气 北京
 2.漏洞扫描
-@me扫描 域名    
+[CQ:emoji,id=9757]@me 扫描 域名    
 3.漏洞查询
-@me查询 (需要使用\\r\\n也就是需要对两个值分成两行)
+[CQ:emoji,id=9757]@me 查询
 Token:XXXXXXXXXXXXXXX\r\nKey:XXXXXXXXX
-4.漏洞个数查询(需要使用\\r\\n也就是需要对两个值分成两行)
-@me查询个数 
+4.漏洞个数查询
+[CQ:emoji,id=9757]@me 查询个数 
 Token:XXXXXXXXXXXXXXX\r\nKey:XXXXXXXXXX
+5.启动监控(需要超级管理员权限
+6.关闭监控
 """
+
+
 
 
 @on_command('help')
 async def BotHelp(session: CommandSession):
     user_qq_id = session.ctx['user_id']#获取用户QQ
     await session.send(message.MessageSegment.at(user_qq_id) + help_list)
+
--- config.py
+++ config.py
@@ -7,3 +7,6 @@ dns_log_key="XXXXXXXXXXXXXXXXXXXXXXXX"
 debug_mode=False
 whitelist_group_status=False#白名单是否开启
 whitelist_group_list=[]#白名单群ID列表
+super_administrator="" #超级管理员,只能一个
+managed_group=[""]#你管理的群列表
+FaceCascadePath="haarcascade_frontalface_default.xml" #人脸识别训练数据

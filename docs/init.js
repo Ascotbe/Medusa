1982a49ecaaa185c96b6eb2cabdd1bb59fa6720e
Ascotbe <869335729@qq.com>
2020-04-02 00:26:49 +0800

    v0.77.3:palm_tree:

--- docs/init.js
+++ docs/init.js
@@ -30,21 +30,7 @@ config.nav.push({
     title: 'Language: ' + versions[currentVersionIndex].title, type: 'dropdown', items: versions
 });
 
-if (currentVersionIndex > latestVersionIndex) {
-    config.announcement = {
-        type: 'danger',
-        html: '你当前正在访问的是旧版本插件的文档，内容可能与最新版本不相符，点击&nbsp;<span style="cursor: pointer;" '
-            + 'onclick="window.location.assign(\'' + versions[latestVersionIndex].path + '\' + window.location.hash)"'
-            + '>这里</span>&nbsp;访问最新文档。'
-    };
-} else if (currentVersionIndex < latestVersionIndex) {
-    config.announcement = {
-        type: 'primary',
-        html: '你当前正在访问的是测试版插件的文档，测试版本提供了更丰富的功能但可能不稳定，点击&nbsp;<span style="cursor: pointer;" '
-            + 'onclick="window.location.assign(\'' + versions[latestVersionIndex].path + '\' + window.location.hash)"'
-            + '>这里</span>&nbsp;访问最新稳定版的文档。'
-    };
-}
+
 
 docute.init(config);
 

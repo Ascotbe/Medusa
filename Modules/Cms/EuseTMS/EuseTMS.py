e4931df948573c384524d1e86fc08687996550ee
KpLi0rn <563164617@qq.com>
2020-05-29 10:34:27 +0800

    v0.83.1:palm_tree:

--- Modules/Cms/EuseTMS/EuseTMS.py
+++ Modules/Cms/EuseTMS/EuseTMS.py
@@ -4,6 +4,9 @@ from Modules.Cms.EuseTMS import EuseTMSSQLInjectionVulnerability
 from Modules.Cms.EuseTMS import EuseTMSSQLInjectionVulnerability1
 from Modules.Cms.EuseTMS import EuseTMSSQLInjectionVulnerability2
 from Modules.Cms.EuseTMS import EuseTMSSQLInjectionVulnerability3
+from Modules.Cms.EuseTMS import EuseTMSSQLInjectionVulnerability4
+from Modules.Cms.EuseTMS import EuseTMSSQLInjectionVulnerability5
+from Modules.Cms.EuseTMS import EuseTMSSQLInjectionVulnerability6
 
 from ClassCongregation import Prompt
 def Main(ThreadPool,Url,Values,proxies,**kwargs):
@@ -11,4 +14,8 @@ def Main(ThreadPool,Url,Values,proxies,**kwargs):
     ThreadPool.Append(EuseTMSSQLInjectionVulnerability1.medusa, Url,Values,proxies=proxies,**kwargs)
     ThreadPool.Append(EuseTMSSQLInjectionVulnerability2.medusa, Url,Values,proxies=proxies,**kwargs)
     ThreadPool.Append(EuseTMSSQLInjectionVulnerability3.medusa, Url,Values,proxies=proxies,**kwargs)
+    ThreadPool.Append(EuseTMSSQLInjectionVulnerability4.medusa, Url,Values,proxies=proxies,**kwargs)
+    ThreadPool.Append(EuseTMSSQLInjectionVulnerability5.medusa, Url,Values,proxies=proxies,**kwargs)
+    ThreadPool.Append(EuseTMSSQLInjectionVulnerability6.medusa, Url,Values,proxies=proxies,**kwargs)
+
     Prompt("EuseTMS")

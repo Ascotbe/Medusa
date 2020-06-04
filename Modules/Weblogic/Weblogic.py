82830e87f0d3dae4900149f59a54b7b7fedd5318
Ascotbe <869335729@qq.com>
2020-06-04 21:52:01 +0800

    v0.84.1:palm_tree:

--- Modules/Weblogic/Weblogic.py
+++ Modules/Weblogic/Weblogic.py
@@ -4,10 +4,27 @@ from Modules.Weblogic import WeblogicServerSideRequestForgeryVulnerability
 from Modules.Weblogic import WeblogicWLSCoreComponentsDeserializationCommandExecutionVulnerability
 from Modules.Weblogic import WebLogicXMLDecoderDeserializationVulnerability
 from Modules.Weblogic import WeblogicArbitraryFileUploadVulnerability
+from Modules.Weblogic import WeblogicDeserializationCommandExecutionVulnerability
+from Modules.Weblogic import WeblogicDeserializationCommandExecutionVulnerability2
+from Modules.Weblogic import WeblogicDeserializationCommandExecutionVulnerability3
+from Modules.Weblogic import WeblogicT3DeserializationCommandExecutionVulnerability
 from ClassCongregation import Prompt
 def Main(ThreadPool,Url,Values,proxies,**kwargs):
     ThreadPool.Append(WeblogicServerSideRequestForgeryVulnerability.medusa,Url, Values, proxies = proxies, ** kwargs)
     ThreadPool.Append(WebLogicXMLDecoderDeserializationVulnerability.medusa, Url, Values, proxies=proxies, **kwargs)
     ThreadPool.Append(WeblogicArbitraryFileUploadVulnerability.medusa, Url, Values, proxies=proxies, **kwargs)
     ThreadPool.Append(WeblogicWLSCoreComponentsDeserializationCommandExecutionVulnerability.medusa, Url, Values, proxies=proxies, **kwargs)
+
+    ThreadPool.Append(WeblogicDeserializationCommandExecutionVulnerability.medusa, Url, Values,
+                      proxies=proxies, **kwargs)
+
+    ThreadPool.Append(WeblogicDeserializationCommandExecutionVulnerability2.medusa, Url, Values,
+                      proxies=proxies, **kwargs)
+
+    ThreadPool.Append(WeblogicDeserializationCommandExecutionVulnerability3.medusa, Url, Values,
+                      proxies=proxies, **kwargs)
+
+    ThreadPool.Append(WeblogicT3DeserializationCommandExecutionVulnerability.medusa, Url, Values,
+                      proxies=proxies, **kwargs)
+
     Prompt("Weblogic")

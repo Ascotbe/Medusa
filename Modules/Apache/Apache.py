6e1904dd6a6aaf4c9b89242078ce4e425304c2ae
Ascotbe <869335729@qq.com>
2020-06-08 16:23:27 +0800

    v0.84.6:palm_tree:

--- Modules/Apache/Apache.py
+++ Modules/Apache/Apache.py
@@ -5,6 +5,7 @@ from Modules.Apache.Flink import Flink
 from Modules.Apache.ActiveMQ import ActiveMQ
 from Modules.Apache.Solr import Solr
 from Modules.Apache.Tomcat import Tomcat
+from Modules.Apache.Shiro import Shiro
 
 def Main(ThreadPool,Url,Values,proxies,**kwargs):
 
@@ -12,4 +13,5 @@ def Main(ThreadPool,Url,Values,proxies,**kwargs):
     ActiveMQ.Main(ThreadPool,Url,Values,proxies,**kwargs)
     Flink.Main(ThreadPool,Url,Values,proxies,**kwargs)
     Tomcat.Main(ThreadPool,Url,Values,proxies,**kwargs)
+    Shiro.Main(ThreadPool, Url, Values, proxies, **kwargs)
     #Log4j.Main(ThreadPool, Url, Values, Token)#暂时注释该插件

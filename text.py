4ac5377c338e76a0af635197468e3fd89579ff40
Ascotbe <869335729@qq.com>
2020-07-07 10:34:59 +0800

    v0.87:palm_tree:

--- text.py
+++ text.py
@@ -3,10 +3,11 @@
 # if __name__ == '__main__':
 #     UrlList=[]
 #     ThredList=[]
+#     la=[]
 #     with open("6.txt", 'r', encoding='UTF-8') as f:
 #         line = f.readline()
 #         while line:
-#             ThredList.append(threading.Thread(target=medusa, args=(line.strip("\r\n",),"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36",)kwargs={"Uid":""Ayanami Rei"","Sid":""Soryu Asuka Langley""}))
+#             ThredList.append(threading.Thread(target=medusa, args=(line.strip("\r\n",),"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36",),kwargs={"Uid":"Ayanami Rei","Sid":"Soryu Asuka Langley"}))
 #             line = f.readline()
 #     for t in ThredList:  # 开启列表中的多线程
 #         t.start()

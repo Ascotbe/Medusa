ea17fb212a339745625fad59a6340f1b95874d4a
Ascotbe <869335729@qq.com>
2020-10-29 14:05:34 +0800

    v0.90.7:palm_tree:

--- Web/Workbench/Tasks.py
+++ Web/Workbench/Tasks.py
@@ -29,7 +29,6 @@ from Modules.Apache.Tomcat import Tomcat
 from Modules.Subdomain.SubdomainSearch import SubdomainSearch
 from ClassCongregation import ProcessPool
 from Web.WebClassCongregation import OriginalProxyData,ActiveScanList
-import time
 from config import proxy_scan_module_list,proxy_scan_process,proxy_scanned_by_proxy
 MedusaVulnerabilityList={
 "Struts2":Struts2.Main,
@@ -83,18 +82,13 @@ def MedusaScan(Url,Module,ScanProcess,Values,proxies,**kwargs):
 
 @app.task
 def ProxyScan(Url,AgentHeader,**kwargs):
-    # ProxuProcessPool = ProcessPool()  # 定义一个线程池
-    # for Module in proxy_scan_module_list:
-    #     try:
-    #         MedusaVulnerabilityList[Module](ProxuProcessPool, Url, AgentHeader, proxy_scanned_by_proxy, **kwargs)  # 调用列表里面的值
-    #     except:  # 如果传入非法字符串会调用出错
-    #         pass
-    #
-    # ProxuProcessPool.Start(proxy_scan_process)
-    # OriginalProxyData().UpdateScanStatus(uid=kwargs.get("Uid"), redis_id=Test.request.id)  # 获取RedisID后进行更新数据库
+    ProxuProcessPool = ProcessPool()  # 定义一个线程池
+    for Module in proxy_scan_module_list:
+        try:
+            MedusaVulnerabilityList[Module](ProxuProcessPool, Url, eval(AgentHeader), proxy_scanned_by_proxy, **kwargs)  # 调用列表里面的值
+        except:  # 如果传入非法字符串会调用出错
+            pass
+    InformationLeakage.ProxyMain(ProxuProcessPool, Url[:-1], eval(AgentHeader), proxy_scanned_by_proxy, **kwargs)#删除url最后一个/符号
+    ProxuProcessPool.Start(proxy_scan_process)
+    OriginalProxyData().UpdateScanStatus(uid=kwargs.get("Uid"), redis_id=ProxyScan.request.id)  # 获取RedisID后进行更新数据库
 
-    print(AgentHeader)
-    print(type(AgentHeader))
-    print(Url)
-    print(type(Url))
-    print(kwargs)

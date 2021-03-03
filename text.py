43e2f5f7c0dd5826ef16302b65124b52c309f002
Ascotbe <869335729@qq.com>
2021-03-03 21:16:19 +0800

    v0.98.1:palm_tree:

--- text.py
+++ text.py
@@ -9,7 +9,15 @@
 # find . -type d -name '__pycache__' | xargs rm -rf
 # pip install python-magic-bin==0.4.14
 
+import time
+StartingTime=time.time()+1
+ZipFilePath="11111"
+ExtractData=["d","ss"]
+FileName="dasdasd"
+print("[ + ] 下载文件：\033[36m" + FileName + "\033[0m 耗时：\033[34m" + str(
+    time.time() - StartingTime) + "S \033[0m")
+print("[ ~ ] 本次写入文件：\033[36m" + ZipFilePath + "\033[0m 耗时：\033[34m" + str(
+    time.time() - StartingTime) + "S \033[0m 数据量：\033[32m" + str(len(ExtractData)) + "\033[0m条")
+print("[ - ] 正在重新下载文件：\033[36m" + FileName + "\033[0m 耗时：\033[34m" + str(
+    time.time() - StartingTime) + "S \033[0m")
 
-
-
-#已知问题CVE数据包下载错误导致有些数据无法导入问题

b587b95cec812190deca59d2c83ad0789bca3440
Ascotbe <869335729@qq.com>
2021-03-04 20:42:40 +0800

    v0.98.2:palm_tree:

--- text.py
+++ text.py
@@ -9,15 +9,5 @@
 # find . -type d -name '__pycache__' | xargs rm -rf
 # pip install python-magic-bin==0.4.14
 
-import time
-StartingTime=time.time()+1
-ZipFilePath="11111"
-ExtractData=["d","ss"]
-FileName="dasdasd"
-print("[ + ] 下载文件：\033[36m" + FileName + "\033[0m 耗时：\033[34m" + str(
-    time.time() - StartingTime) + "S \033[0m")
-print("[ ~ ] 本次写入文件：\033[36m" + ZipFilePath + "\033[0m 耗时：\033[34m" + str(
-    time.time() - StartingTime) + "S \033[0m 数据量：\033[32m" + str(len(ExtractData)) + "\033[0m条")
-print("[ - ] 正在重新下载文件：\033[36m" + FileName + "\033[0m 耗时：\033[34m" + str(
-    time.time() - StartingTime) + "S \033[0m")
-
+ZipFilePath="/Users/ascotbe/code/Medusa/Temp/nvdcve-1.1-2002.json.zip"
+print("[ + ] 初始化文件失败正在重新下载：\033[36m" +ZipFilePath[-13:-9]+ "dasda"+ZipFilePath[:-24]+"\033[0m")

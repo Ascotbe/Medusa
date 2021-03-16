16ba4757e3dd0c928e8bec3f9139f6c1437f2f03
Ascotbe <869335729@qq.com>
2021-03-16 15:09:06 +0800

    v0.98.3:palm_tree:

--- text.py
+++ text.py
@@ -8,6 +8,3 @@
 # git commit -m  "v0.82.3:palm_tree:"
 # find . -type d -name '__pycache__' | xargs rm -rf
 # pip install python-magic-bin==0.4.14
-
-ZipFilePath="/Users/ascotbe/code/Medusa/Temp/nvdcve-1.1-2002.json.zip"
-print("[ + ] 初始化文件失败正在重新下载：\033[36m" +ZipFilePath[-13:-9]+ "dasda"+ZipFilePath[:-24]+"\033[0m")

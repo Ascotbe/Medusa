e930e1cd56c3254c5c8d88989aac3110817a4a98
Ascotbe <869335729@qq.com>
2021-04-27 16:14:29 +0800

    v1.0.1:palm_tree:

--- Modules/Weblogic/Weblogic.py
+++ Modules/Weblogic/Weblogic.py
@@ -7,7 +7,7 @@ from Modules.Weblogic import WeblogicArbitraryFileUploadVulnerability
 from Modules.Weblogic import WeblogicDeserializationCommandExecutionVulnerability
 from Modules.Weblogic import WeblogicDeserializationCommandExecutionVulnerability2
 from Modules.Weblogic import WeblogicDeserializationCommandExecutionVulnerability3
-from Modules.Weblogic import WeblogicT3DeserializationCommandExecutionVulnerability
+#from Modules.Weblogic import WeblogicT3DeserializationCommandExecutionVulnerability
 from Modules.Weblogic import WebLogicRemoteCommandExecutionVulnerability
 from Modules.Weblogic import WebLogicDeserializationRemoteCommandExecutionVulnerability
 from Modules.Weblogic import WeblogicDeserializationCommandExecutionVulnerability4
@@ -21,7 +21,7 @@ def Main(Pool,**kwargs):
     Pool.Append(WeblogicDeserializationCommandExecutionVulnerability.medusa,  **kwargs)
     Pool.Append(WeblogicDeserializationCommandExecutionVulnerability2.medusa,  **kwargs)
     Pool.Append(WeblogicDeserializationCommandExecutionVulnerability3.medusa,  **kwargs)
-    Pool.Append(WeblogicT3DeserializationCommandExecutionVulnerability.medusa,  **kwargs)
+    #Pool.Append(WeblogicT3DeserializationCommandExecutionVulnerability.medusa,  **kwargs)
     Pool.Append(WebLogicRemoteCommandExecutionVulnerability.medusa,  **kwargs)
     Pool.Append(WebLogicDeserializationRemoteCommandExecutionVulnerability.medusa,  **kwargs)
     Pool.Append(WeblogicDeserializationCommandExecutionVulnerability4.medusa,  **kwargs)

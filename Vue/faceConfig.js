cb8cad212ab1e5791fd4b324a60f27c70343f7b1
ascotbe <869335729@qq.com>
2021-07-02 13:53:15 +0800

    v1.0.12:palm_tree:

--- Vue/faceConfig.js
+++ Vue/faceConfig.js
@@ -3,8 +3,8 @@ const { baseURL } = require("./src/utils/request")
 const faceConfig = () =>{
     return{
         // 基础路径,打包发布的时候修改为后端发布服务地址
-        basePath: 'http://127.0.0.1:9999/api/',
-        imgPath:'http://127.0.0.1:9999/s/'
+        basePath: 'http://101.37.14.144:8888/api/',
+        imgPath:'http://101.37.14.144:8888/s/'
     }
 }
 

